05c4dd4141babbac6b5cf006fa3b6eff7717555c
rabinsen <rivansen0071@gmail.com>
2022-01-27 17:32:08 -0600

    Vehicles configuration

--- core/src/main/java/com/grabRental/cs544/model/Vehicle.java
+++ core/src/main/java/com/grabRental/cs544/model/Vehicle.java
@@ -1,4 +1,41 @@
 package com.grabRental.cs544.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Vehicle {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String numberPlate;
+    private String model;
+    private String brand;
+    private int modelYear;
+    private int numberOfSeats;
+    private String insuranceId;
+    private Boolean pickUpSpace;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    public Vehicle(String numberPlate, String model, String brand, int modelYear, int numberOfSeats, String insuranceId, Boolean pickUpSpace, Date createdAt)
+    {
+        this.numberPlate = numberPlate;
+        this.model = model;
+        this.brand = brand;
+        this.modelYear = modelYear;
+        this.numberOfSeats = numberOfSeats;
+        this.insuranceId = insuranceId;
+        this.pickUpSpace = pickUpSpace;
+        this.createdAt = createdAt;
+    }
 }
--- vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
+++ vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
@@ -2,6 +2,7 @@ package com.grabrental.cs544.vehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VehicleApplication {
