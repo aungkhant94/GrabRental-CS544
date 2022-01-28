79ad7dd95ee72fa5aefe6aa742634e60c7bf9921
rabinsen <rivansen0071@gmail.com>
2022-01-28 08:25:28 -0600

    swagger added

--- vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
+++ vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
@@ -6,15 +6,31 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan("com.grabRental.cs544.*")
-
 @EnableJpaRepositories
+@EnableSwagger2
 public class VehicleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.grabrental.cs544.vehicle"))
+				.paths(PathSelectors.regex("/vehicles.*"))
+				.build();
+
 	}
 
 }
