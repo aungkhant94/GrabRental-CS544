ffa773499df18d120ddf00c393c39b2863c349c5
Aung Khant <aungkhant94@gmail.com>
2022-01-27 20:44:42 -0600

    myfiles

--- core/src/main/java/com/grabRental/cs544/dto/ScheduleDTO.java
+++ core/src/main/java/com/grabRental/cs544/dto/ScheduleDTO.java
@@ -1,5 +1,21 @@
 package com.grabRental.cs544.dto;
 
+import java.util.Date;
+
+import com.grabRental.cs544.enumeration.Location;
+
 public class ScheduleDTO {
 
+	private Long id;
+	
+	private Date date;
+	
+	
+	private Integer durationInMinute;
+	
+	private Integer noOfPassenger;
+	
+	private Location source;
+	
+	private Location destination;
 }
