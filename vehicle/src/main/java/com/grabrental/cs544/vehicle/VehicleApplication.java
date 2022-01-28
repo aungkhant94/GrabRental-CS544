d677f14d9d49d087dddb51ba75edf72a8f357969
unknown <basnayaka.duminda@gmail.com>
2022-01-28 08:22:18 -0600

    sssssss

--- vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
+++ vehicle/src/main/java/com/grabrental/cs544/vehicle/VehicleApplication.java
@@ -3,13 +3,14 @@ package com.grabrental.cs544.vehicle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.grabRental.cs544.*")
-
+@EnableDiscoveryClient
 @EnableJpaRepositories
 public class VehicleApplication {
 
