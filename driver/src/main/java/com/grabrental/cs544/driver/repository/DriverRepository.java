dd248d662b846e246b825283dd26b8e3d4f62f38
Aung Khant <aungkhant94@gmail.com>
2022-01-29 20:54:54 -0600

    final

--- driver/src/main/java/com/grabrental/cs544/driver/repository/DriverRepository.java
+++ driver/src/main/java/com/grabrental/cs544/driver/repository/DriverRepository.java
@@ -13,11 +13,15 @@ import java.util.List;
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
-    @Query(value = "select * from driver d1 where d1.id not in " +
+    /* @Query(value = "select * from driver d1 where d1.id not in " +
             "(select svh.vehicle_id from service_vehicle svh  " +
             "join driver d2 on d2.id = svh.vehicle_id " +
             "join schedule sch on sch.id = svh.schedule_id " +
             "where sch.date BETWEEN ?1 AND ?2 " +
-            "OR sch.end_date BETWEEN ?1 AND ?2) ", nativeQuery = true)
+            "OR sch.end_date BETWEEN ?1 AND ?2) ", nativeQuery = true) */
+    @Query("SELECT dr FROM Driver dr WHERE dr.id NOT IN" +
+            "(SELECT d.id FROM ServiceVehicle svh JOIN svh.schedule sc JOIN svh.driver d " +
+            "WHERE(sc.date BETWEEN ?1 AND ?2)" +
+            "OR sc.endDate BETWEEN ?1 AND ?2)")
     List<Driver> getAvailableDrivers(@Temporal Date startTime, @Temporal Date endTime);
 }
